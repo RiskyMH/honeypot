2a258cc579ad6af47ea3d626b20b5cc646bad437
RiskyMH <git@riskymh.dev>
2026-08-05 22:49:55 +1000

    improve node modules size even more make discord-api-types tree shake & get async event emmiter from github and use normal node inspect (instead of pollyfill) to improve bundle size

--- src/utils/initial-presence.ts
+++ src/utils/initial-presence.ts
@@ -1,16 +1,14 @@
-import { ActivityType, PresenceUpdateStatus } from "discord-api-types/v10";
+import { ActivityType, PresenceUpdateStatus, type GatewayPresenceUpdateData } from "discord-api-types/v10";
 
 const initialPresence = {
     since: null,
-    activities: [
-        {
-            name: "#honeypot",
-            state: "Watching #honeypot for bots",
-            type: ActivityType.Custom,
-        }
-    ],
+    activities: [{
+        name: "#honeypot",
+        state: "Watching #honeypot for bots",
+        type: ActivityType.Custom,
+    }],
     status: PresenceUpdateStatus.Online,
     afk: false,
-}
+} satisfies GatewayPresenceUpdateData;
 
 export default initialPresence;
